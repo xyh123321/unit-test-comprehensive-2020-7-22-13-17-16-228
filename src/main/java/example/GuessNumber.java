ea8cfb227d2197cde09277487058e7c226ed0bf7
JAYDEN Y H XIE (ITA-ISDC-ISD-OOCLL/ZHA) <XIEJA3@OOCL.COM>
2020-07-23 18:32:04 +0800

    [guess number][jayden] feature:pass should_return_4A0B_when_guess_number_given_1234

--- src/main/java/example/GuessNumber.java
+++ src/main/java/example/GuessNumber.java
@@ -3,6 +3,16 @@ package example;
 public class GuessNumber {
 
     public String guess(int[] inputNumber, int[] answer) {
+        boolean isAllCorrect = true;
+        for (int i = 0; i < inputNumber.length; i++) {
+            if(inputNumber[i] != answer[i]) {
+                isAllCorrect = false;
+                break;
+            }
+        }
+        if(isAllCorrect) {
+            return "4A0B";
+        }
         return null;
     }
 }
